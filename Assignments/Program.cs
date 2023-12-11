9ff537ed9ff6ce0d7c5d2cd7ad27657e6238d691
ShilpaChandrashekar21 <Shilpa.Chandrashekar@ust.com>
2023-12-11 16:20:05 +0530

    Add project files.

---
+++ Assignments/Program.cs
@@ -0,0 +1,94 @@
+﻿
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System.Runtime.InteropServices.JavaScript;
+
+string baseUrl = "https://jsonplaceholder.typicode.com/";
+var client = new RestClient(baseUrl);
+
+GetAllPost(client);
+GetOnePost(client);
+GetAllComments(client);
+GetCommentsforOnePost(client);
+CreatePost(client);
+UpdatePost(client);
+DeletePost(client);
+
+static void GetAllPost(RestClient client)
+{
+    var getAllPostRequest = new RestRequest("posts", Method.Get);
+    var getAllPostResponse = client.Execute(getAllPostRequest);
+    //JObject response = JObject.Parse(getAllPostResponse.Content);
+    Console.WriteLine("Get All Post response: " + getAllPostResponse.Content);
+
+}
+
+static void GetOnePost(RestClient client)
+{
+    var getOnePostRequest = new RestRequest("posts/1", Method.Get);
+    var getOnePostResponse = client.Execute(getOnePostRequest);
+
+    JObject response = JObject.Parse(getOnePostResponse.Content);
+    Console.WriteLine("Get One Post response: " + response);
+}
+
+static void GetAllComments(RestClient client)
+{
+    var getAllCommentsRequest = new RestRequest("posts/1/comments", Method.Get);
+    var getAllCommentsResponse = client.Execute(getAllCommentsRequest);
+
+    //JObject response = JObject.Parse(getAllCommentsResponse.Content);
+    Console.WriteLine("Get All Comment response: " + getAllCommentsResponse.Content);
+}
+
+static void GetCommentsforOnePost(RestClient client)
+{
+    var getOneCommentRequest = new RestRequest("comments", Method.Get);
+    getOneCommentRequest.AddQueryParameter("postId", "1");
+    var getOneCommentResponse = client.Execute(getOneCommentRequest);
+
+    JObject response = JObject.Parse(getOneCommentResponse.Content);
+    Console.WriteLine("Get Comment for One Post response: " + response);
+
+}
+
+static void CreatePost(RestClient client)
+{
+    var createPostRequest = new RestRequest("posts", Method.Post);
+    createPostRequest.AddHeader("Content-Type", "application/json");
+    createPostRequest.AddJsonBody(new
+    {
+        userId = 1,
+        id = 1,
+        title = "comics",
+        body = "empty"
+    });
+
+    var createPostResponse = client.Execute(createPostRequest);
+    JObject response = JObject.Parse(createPostResponse.Content);
+    Console.WriteLine("Create Post response: " + response);
+}
+
+static void UpdatePost(RestClient client)
+{
+    var updatePostRequest = new RestRequest("posts/1", Method.Put);
+    updatePostRequest.AddHeader("Content-Type", "application/json");
+    updatePostRequest.AddJsonBody(new
+    {
+        userId = 2,
+        id = 12,
+        title = "comics",
+        body = "empty"
+    });
+    var updatePostResponse = client.Execute(updatePostRequest);
+    JObject response = JObject.Parse(updatePostResponse.Content);
+    Console.WriteLine("Update Post response: " + response);
+}
+
+static void DeletePost(RestClient client)
+{
+    var deletePostRequest = new RestRequest("posts/1", Method.Delete);
+    var deletePostResponse = client.Execute(deletePostRequest);
+    Console.WriteLine("Delete Post response: " + deletePostResponse.Content);
+}
+
---
+++ RestExamples/Program.cs
@@ -0,0 +1,135 @@
+﻿
+using Newtonsoft.Json.Linq;
+using RestSharp;
+
+string baseUrl = "https://reqres.in/api/";
+var client = new RestClient(baseUrl);
+
+//get
+var getUserRequest = new RestRequest("users/2", Method.Get);
+
+var getUserResponse = client.Execute(getUserRequest);
+Console.WriteLine("\nGet response");
+Console.WriteLine(getUserResponse.Content);
+
+//post
+var createUserRequest = new RestRequest("users", Method.Post);
+createUserRequest.AddParameter("name", "John Doe");
+createUserRequest.AddParameter("job", "Developer");
+
+var createUserResponse = client.Execute(createUserRequest);
+Console.WriteLine("\nPost response");
+Console.WriteLine(createUserResponse.Content);
+
+//put
+var updateUserRequest = new RestRequest("users/2", Method.Put);
+updateUserRequest.AddParameter("name", "Johnny");
+updateUserRequest.AddParameter("job", "Artist");
+
+var updateUserResponse = client.Execute(updateUserRequest);
+Console.WriteLine("\nPut response");
+Console.WriteLine(updateUserResponse.Content);
+
+//delete
+
+var deleteUserRequest = new RestRequest("users/2", Method.Delete);
+
+var deleteUserResponse = client.Execute(deleteUserRequest);
+Console.WriteLine("\nDelete response");
+Console.WriteLine(deleteUserResponse.ResponseStatus);
+
+
+var getUsersRequest = new RestRequest("users/2", Method.Get);
+var getUsersResponse = client.Execute(getUsersRequest);
+//response in json format
+
+if (getUsersResponse.StatusCode == System.Net.HttpStatusCode.OK)
+{
+    JObject? userJson = JObject.Parse(getUsersResponse.Content);
+    // string? pageno = userJson["page"]?.ToString();
+    string? userFirstName = userJson["data"]?["first_name"]?.ToString();
+    string? userLastName = userJson["data"]?["last_name"]?.ToString();
+    Console.WriteLine(userJson);
+    Console.WriteLine($"FirstName - {userFirstName},LastName - {userLastName}");
+
+}
+else
+{
+    Console.WriteLine($"Error - {getUsersResponse.ErrorMessage}");
+}
+/*Console.WriteLine("\nGet response for page 1");
+Console.WriteLine(getUsersResponse.Content);
+*/
+
+//get with query parameter
+
+GetAllUsers(client);
+GetSingleUser(client);
+CreateUser(client);
+UpdateUser(client);
+
+static void GetAllUsers(RestClient client)
+{
+    var getUsersRequest = new RestRequest("users", Method.Get);
+    getUsersRequest.AddQueryParameter("page", "1");
+    getUsersRequest.AddHeader("Content-Type", "application/json");
+    var getUsersResponse = client.Execute(getUsersRequest);
+    JObject? response = JObject.Parse(getUsersResponse.Content);
+
+    Console.WriteLine("\n method Get response for page 1");
+    Console.WriteLine(response);
+}
+
+static void GetSingleUser(RestClient client)
+{
+    var getUsersRequest = new RestRequest("users/2", Method.Get);
+
+    var getUsersResponse = client.Execute(getUsersRequest);
+    JObject response = JObject.Parse(getUsersResponse.Content);
+    Console.WriteLine("\n method Get response");
+    Console.WriteLine(response);
+}
+
+static void CreateUser(RestClient client)
+{
+    var createUsersRequest = new RestRequest("users", Method.Post);
+    createUsersRequest.AddHeader("Content-Type", "application/json");
+    createUsersRequest.AddJsonBody(new { name = "John-Doe", job = "Developer" });
+
+    var createUsersResponse = client.Execute(createUsersRequest);
+    Console.WriteLine("\n method Post response for response body");
+    Console.WriteLine(createUsersResponse.Content);
+
+}
+//post with request body
+var createUsersRequest = new RestRequest("users", Method.Post);
+createUsersRequest.AddHeader("Content-Type", "application/json");
+createUsersRequest.AddJsonBody(new { name = "John-Doe", job = "Developer" });
+
+
+var createUsersResponse = client.Execute(createUsersRequest);
+Console.WriteLine("\nPost response for response body");
+Console.WriteLine(createUsersResponse.Content);
+
+//put with response body
+static void UpdateUser(RestClient client)
+{
+    var updateUsersRequest = new RestRequest("users/2", Method.Put);
+    updateUsersRequest.AddHeader("Content-Type", "application/json");
+    updateUsersRequest.AddJsonBody(new { name = "Johnny", job = "Artist" });
+
+    var updateUsersResponse = client.Execute(updateUsersRequest);
+    Console.WriteLine("\n method Put with response body");
+    Console.WriteLine(updateUsersResponse.Content);
+
+}
+
+var updateUsersRequest = new RestRequest("users/2", Method.Put);
+updateUsersRequest.AddHeader("Content-Type", "application/json");
+updateUsersRequest.AddJsonBody(new { name = "Johnny", job = "Artist" });
+
+var updateUsersResponse = client.Execute(updateUsersRequest);
+Console.WriteLine("\n method Put with response body");
+Console.WriteLine(updateUsersResponse.Content);
+
+
